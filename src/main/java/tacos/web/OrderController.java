4e5f7a49ad6f0a72bdd605feac99e09036a09c65
Ashish Ambre <a.ambre06@gmail.com>
2022-01-10 09:03:46 +0530

    Added basic controllers and validations

---
+++ src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,33 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.TacoOrder;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+  @GetMapping("/current")
+  public String orderForm(Model model) {
+    model.addAttribute("tacoOrder", new TacoOrder());
+    return "orderForm";
+  }
+
+  @PostMapping
+  public String processOrder(@Valid TacoOrder order, Errors errors) {
+    if(errors.hasErrors()) {
+      return "orderForm";
+    }
+    log.info("Order submitted: " + order);
+    return "redirect:/";
+  }
+}
